9d6965de1f43b17090d134c2a4d6d178c6033621
Sma1lboy <541898146chen@gmail.com>
2023-11-11 14:04:01 -0600

    adding content script

---
+++ scripts/content.js
@@ -0,0 +1,37 @@
+
+initYoutube()
+
+function initYoutube() {
+    waitForElement("ytd-watch-flexy").then((res) => {
+    
+        a = document.getElementById("secondary")
+        console.log(a)
+
+    })
+}
+
+function waitForElement(selector) {
+    return new Promise(resolve => {
+        // Check if the element already exists
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        // If the element doesn't exist, set up a MutationObserver to watch for changes
+        const observer = new MutationObserver(mutations => {
+            // Check if the element now exists
+            if (document.querySelector(selector)) {
+                // Resolve the promise with the element
+                resolve(document.querySelector(selector));
+                // Disconnect the observer to stop watching for changes
+                observer.disconnect();
+            }
+        });
+
+        // Start observing the body for changes (childList and subtree)
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
