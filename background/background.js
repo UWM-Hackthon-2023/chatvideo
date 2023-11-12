61a1e827105e13d7c79fcb114d96585c0766cf86
Sma1lboy <541898146chen@gmail.com>
2023-11-12 01:12:09 -0600

    summary to chatgpt

--- background/background.js
+++ background/background.js
@@ -1,31 +1,11 @@
-// 在 background.js 或其他脚本中
-//background.js
+let prompt = "";
 
-function fetchVideoSummary(videoDescription) {
-    const apiKey = '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'; // 替换为你的OpenAI API密钥
-    const url = 'https://api.openai.com/v1/engines/davinci-codex/completions'; // GPT-3 API URL
-
-    // GPT-3的请求数据
-    const data = {
-        prompt: "Write a summary about this video: " + videoDescription,
-        max_tokens: 100
-    };
-
-    // 使用 fetch 发送 API 请求
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${apiKey}`
-        },
-        body: JSON.stringify(data)
-    })
-        .then(response => response.json())
-        .then(data => {
-            // 处理GPT-3的响应数据
-            console.log(data.choices[0].text); // 打印出生成的视频梗概
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
-}
+// On Message
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.message === "setPrompt") {
+        prompt = request.prompt;
+    } else if (request.message === "getPrompt") {
+        sendResponse({ prompt: prompt });
+        prompt = ""; // Reset prompt
+    }
+});
--- scripts/content.js
+++ scripts/content.js
@@ -1,38 +1,51 @@
-
+"use strict";
 
 start()
-async function start() {
-    if(window.location.hostname === "www.youtube.com") {
-        console.log("get into youtube...")
-        await initYoutube()
-    } else {
 
+function start() {
+    window.onload = async () => {
+        if (window.location.hostname === "www.youtube.com") {
+            console.log("get into youtube...")
+            await initYoutube()
+        }
+        if (window.location.hostname === "chat.openai.com") {
+            {
+                if (document.getElementsByTagName("textarea")[0]) {
+                    if (window.location.search === "?ref=chatvideo") {
+                        console.log("get into chatgpt...")
+
+                        chrome.runtime.sendMessage({message: "getPrompt"}, async (response) => {
+                            document.getElementsByTagName("textarea")[0].focus()
+                            // console.log(response.prompt)
+                            copyTextToClipboard(response.prompt)
+                            // document.getElementsByTagName("textarea")[0].value = response.prompt
+
+                            let counter = 0
+                            let input = setInterval(() => {
+                                document.getElementsByTagName("textarea")[0].value = response.prompt
+                                document.getElementsByTagName("button")[document.getElementsByTagName("button").length - 2].disabled = false
+                                console.log("done")
+                                document.getElementsByTagName("button")[document.getElementsByTagName("button").length - 2].click();
+                                counter++
+                                if (counter == 3) {
+                                    clearInterval(input)
+                                }
+                            }, 1000)
+
+                        })
+
+                    }
+                }
+            }
+        }
     }
-
 }
 
+
 async function initYoutube() {
-
-
     waitForElement("ytd-watch-flexy").then((res) => {
-
         initYoutubeBlock(document.getElementById("secondary-inner"))
     })
-
-    const videoId = getPara(window.location.href).v
-    let langOptionsWithLink = await getLangOptionsWithLink(videoId)
-    if (langOptionsWithLink === undefined) {
-        return;
-    }
-
-    let captions = await getCaptionsCollection(langOptionsWithLink)
-
-    let summaryBut = document.getElementById("vyb-block-header-button-summary")
-    summaryBut.addEventListener("click", e => {
-        aggregateCaptions(captions)
-        window.open("https://chat.openai.com/", "_blank")
-    })
-
 }
 
 
@@ -41,15 +54,23 @@ async function getLangOptionsWithLink(videoId) {
     const videoPageHtml = await videoPageResponse.text();
     const splittedHtml = videoPageHtml.split('"captions":')
 
-    if (splittedHtml.length < 2) { return; } // No Caption Available
+    if (splittedHtml.length < 2) {
+        return;
+    } // No Caption Available
 
     const captions_json = JSON.parse(splittedHtml[1].split(',"videoDetails')[0].replace('\n', ''));
     const captionTracks = captions_json.playerCaptionsTracklistRenderer.captionTracks;
-    const languageOptions = Array.from(captionTracks).map(i => { return i.name.simpleText; })
+    const languageOptions = Array.from(captionTracks).map(i => {
+        return i.name.simpleText;
+    })
 
     const first = "English"; // Sort by English first
-    languageOptions.sort(function (x, y) { return x.includes(first) ? -1 : y.includes(first) ? 1 : 0; });
-    languageOptions.sort(function (x, y) { return x == first ? -1 : y == first ? 1 : 0; });
+    languageOptions.sort(function (x, y) {
+        return x.includes(first) ? -1 : y.includes(first) ? 1 : 0;
+    });
+    languageOptions.sort(function (x, y) {
+        return x == first ? -1 : y == first ? 1 : 0;
+    });
 
     return Array.from(languageOptions).map((langName, index) => {
         const link = captionTracks.find(i => i.name.simpleText === langName).baseUrl;
@@ -59,6 +80,7 @@ async function getLangOptionsWithLink(videoId) {
         }
     })
 }
+
 async function getCaptionsCollection(langOptionsWithLink) {
     let captionsResponse = await fetch(langOptionsWithLink[0].link)
     if (!captionsResponse.ok) {
@@ -70,6 +92,7 @@ async function getCaptionsCollection(langOptionsWithLink) {
     let captionsCollections = captionsXML.getElementsByTagName("text")
     return Array.from(captionsCollections)
 }
+
 function getPara(str) {
     let urlObj = new URL(str);
     let paramObj = {};
@@ -79,7 +102,7 @@ function getPara(str) {
     return paramObj
 }
 
-function initYoutubeBlock(secondary) {
+async function initYoutubeBlock(secondary) {
 
     var youtubeBlock = document.createElement("div")
     youtubeBlock.id = "video-youtube-block-container"
@@ -102,26 +125,86 @@ function initYoutubeBlock(secondary) {
     secondary.insertBefore(youtubeBlock, secondary.firstChild)
 
 
+    const videoId = getPara(window.location.href).v
+    let langOptionsWithLink = await getLangOptionsWithLink(videoId)
+    if (langOptionsWithLink === undefined) {
+        return;
+    }
+
+    let captions = await getCaptionsCollection(langOptionsWithLink)
+
+    let summaryBut = document.getElementById("vyb-block-header-button-summary")
+    summaryBut.addEventListener("click", e => {
+        let prompt = getSummaryPrompt(aggregateCaptions(captions))
+        console.log(prompt)
+        chrome.runtime.sendMessage({message: "setPrompt", prompt: prompt});
+        console.log("prompt sent it")
+        setTimeout(() => {
+            chrome.runtime.sendMessage({message: "setPrompt", prompt: prompt});
+            window.open("https://chat.openai.com/chat?ref=chatvideo", "_blank")
+        }, 500);
+
+    })
+
     // 获取标题元素
     var block_wrap = document.getElementById('block_wrap')
     //给标题元素添加点击事件，通过点击控制class的添加&去除达成动画效果
 }
+
+function getSummaryPrompt(text) {
+    return `Video Topic: ${document.title.replace(/\n+/g, " ")};
+     Transcript: ${trucateCaptions(text).replace(/\n+/g, " ").trim()}}; Video Summary: `
+}
+
+//mention that gpt has char buffer limit
+const gptBuffer = 13000
+
+function trucateCaptions(transcript) {
+    if (transcript.length > gptBuffer) {
+        return transcript.substring(0, 13000)
+    }
+    return transcript
+}
+
 function aggregateCaptions(captions) {
     let str = ""
     captions.forEach((a) => str += a.innerHTML + "")
-    // TODO clipboard need to fix it
-    if(navigator.clipboard) {
+    return str;
+}
 
-       console.log(navigator.clipboard)
-    }
-    window.navigator.clipboard.writeText("asdasd")
-        .then(() => {
-            console.log("Text copied to clipboard successfully!");
-        })
-        .catch(err => {
-            console.error("Failed to copy text: ", err);
+//according to
+function copyTextToClipboard(text) {
+
+    if (!navigator.clipboard) {
+        fallbackCopyTextToClipboard(text);
+        return;
+    } else {
+        navigator.clipboard.writeText(text).then(function () {
+        }, function (err) {
         });
+    }
 
+    function fallbackCopyTextToClipboard(text) {
+        var textArea = document.createElement("textarea");
+        textArea.value = text;
+
+        // Avoid scrolling to bottom
+        textArea.style.top = "0";
+        textArea.style.left = "0";
+        textArea.style.position = "fixed";
+
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+
+        try {
+            var successful = document.execCommand('copy');
+            var msg = successful ? 'successful' : 'unsuccessful';
+        } catch (err) {
+        }
+
+        document.body.removeChild(textArea);
+    }
 }
 
 function waitForElement(selector) {
