9c5ce1f9df186da7030556ad670be5eca833bc82
Sma1lboy <541898146chen@gmail.com>
2023-11-11 20:35:46 -0600

    fix some style issue, put on css file

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,5 @@
 // import "./content.css"
-
+chrome.script
 initYoutube()
 async function getLangOptionsWithLink(videoId) {
     const videoPageResponse = await fetch("https://www.youtube.com/watch?v=" + videoId);
@@ -11,22 +11,22 @@ async function getLangOptionsWithLink(videoId) {
     const captions_json = JSON.parse(splittedHtml[1].split(',"videoDetails')[0].replace('\n', ''));
     const captionTracks = captions_json.playerCaptionsTracklistRenderer.captionTracks;
     const languageOptions = Array.from(captionTracks).map(i => { return i.name.simpleText; })
-    
+
     const first = "English"; // Sort by English first
-    languageOptions.sort(function(x,y){ return x.includes(first) ? -1 : y.includes(first) ? 1 : 0; });
-    languageOptions.sort(function(x,y){ return x == first ? -1 : y == first ? 1 : 0; });
+    languageOptions.sort(function (x, y) { return x.includes(first) ? -1 : y.includes(first) ? 1 : 0; });
+    languageOptions.sort(function (x, y) { return x == first ? -1 : y == first ? 1 : 0; });
 
     return Array.from(languageOptions).map((langName, index) => {
         const link = captionTracks.find(i => i.name.simpleText === langName).baseUrl;
         return {
-        language: langName,
-        link: link
+            language: langName,
+            link: link
         }
     })
 }
 async function getCaptionsCollection(videoId) {
     captionsResponse = await fetch(langOptionsWithLink[0].link)
-    if(!captionsResponse.ok) {
+    if (!captionsResponse.ok) {
         throw new Error(`HTTP error! status: ${captionsResponse.status}`)
     }
     parser = new DOMParser()
@@ -45,121 +45,51 @@ function getPara(str) {
 }
 async function initYoutube() {
 
-    
+
     waitForElement("ytd-watch-flexy").then((res) => {
 
-        initYoutubeBlock(document.getElementById("secondary-inner"))  
+        initYoutubeBlock(document.getElementById("secondary-inner"))
     })
 
     const videoId = getPara(window.location.href).v
     langOptionsWithLink = await getLangOptionsWithLink(videoId)
-    if(!langOptionsWithLink) {
+    if (!langOptionsWithLink) {
         return;
     }
 
     captions = await getCaptionsCollection(videoId)
     console.log(captions)
-    
+
 }
 
 function initYoutubeBlock(secondary) {
 
-    var youtubeBlock  = document.createElement("div")
+    var youtubeBlock = document.createElement("div")
     youtubeBlock.id = "video-youtube-block-container"
-    youtubeBlock.style = "width: auto; border: 1px solid #e0e0e0; padding: 10px; margin-bottom: 10px; border-radius: 5px;"
 
 
     youtubeBlock.innerHTML = `
-    <style type="text/css">
-        .block_wrap {
-            width: 500px;
-            margin: 0 auto;
-            border: 1px solid #e3e3e3;
-            border-radius: 10px;
-        }
-        .chapter_wrap {
-            background: white;
-            text-align: left;
-            border-radius: 8px;
-            color: #333333;
-            margin-bottom: 15px;
-            font-size: 14px;
-            overflow: hidden;
-        }
-        .title_item_wrap {
-            padding: 10px 10px 10px 0;
-            margin: 0 10px 0 10px;
-            border-bottom: none;
-            display: flex;
-            align-items: center;
-        }
-        /*使用伪类进行图标绘画*/
-        .title_item_wrap::after {
-            content: '';
-            width: 10px;
-            height: 10px;
-            border-top: 2px solid #999999;
-            border-right: 2px solid #999999;
-            transform: rotate(-45deg);
-            display: inline-block;
-            transition: 0.3s;
-            float: right;
-            margin-top: 10px;
-        }
-        /*使用类acitve类控制图标的旋转和展开时标题的下边界*/
-        .active {
-            border-bottom: 1px solid #000;
-        }
-        .active::after{
-            transform: rotate(135deg);
-            margin-top: 5px;
-        }
-        .chapter_title {
-            font-size: 16px;
-            padding: 0;
-            margin: 0;
-            width: calc(100% - 30px);
-        }
-        .node_wrap {
-            overflow: hidden;
-            overflow-y: scroll;
-            transition: 0.3s;
-        }
-        .node_wrap p {
-            padding: 0 20px 5px 20px;
-            margin: 10px 0;
-            border-bottom: 1px solid #e3e3e3
-        }
-        
-        /*控制内容块隐藏 隐藏时，整块向左边平移200%的宽度，并且将最大高度设置为0，否则页面会留有空白*/
-        .node_wrap_hide {
-            max-height: 0;
-        }
-        /*控制内容块显示，显示时，整块向右边复原，并且将最大高度设置为300px，里面的内容即会将块撑开*/
-        .node_wrap_show {
-            max-height: 150px;
-        }
-    </style>
-		    <div class="block_wrap">
-        <div id="block_wrap" class="title_item_wrap active">
+	<div class="block_wrap" style="">
             <p class="chapter_title">章节名称</p>
-          <button id="vyb-block-header-button-summary" >
-                Summary
-               </button>
-          
-        </div>
-      
-        <div id="list_wrap" class="node_wrap node_wrap_show">
-				<p>123</p>
-        </div>
+            <div style="display: flex; flex-direction: row; justify-content: space-between;">
+                <button id="vyb-block-header-button-summary" >
+                    Summary
+                </button>
+                <button id="block_wrap" class="title_item_wrap active" style="background: transparent;border: 0px;"/>
+            </div>
+    </div>
+    
+    <div id="list_wrap" class="node_wrap node_wrap_show">
+            <p>123</p>
+    </div>
     </div>
     `
-	secondary.insertBefore(youtubeBlock, secondary.firstChild)
+    secondary.insertBefore(youtubeBlock, secondary.firstChild)
     // 获取标题元素
     var block_wrap = document.getElementById('block_wrap')
     console.log(block_wrap)
     //给标题元素添加点击事件，通过点击控制class的添加&去除达成动画效果
-    block_wrap.onclick = function() {
+    block_wrap.onclick = function () {
         // 获取标题元素className集合
         let classArray = this.className.split(/\s+/)
 
